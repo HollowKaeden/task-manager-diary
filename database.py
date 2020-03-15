ce8d76ef2094c4c12889ad2ffe1994d2d6da03ff
RuletkaDudo <61617281+RuletkaDudo@users.noreply.github.com>
2020-03-15 11:53:06 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,29 @@
 # Запросы базе данных
+import sqlite3
+cursor = None
+conn = None
+
+
+def connect():
+    global cursor, conn
+    conn = sqlite3.connect("task_manager_data.db")
+    cursor = conn.cursor()
+
+
+connect()
+
+
+def add_events(name):
+    cursor.execute('INSERT INTO EVENTS (name) VALUES (?)', (name,))
+    conn.commit()
+
+
+def del_events(name):
+    cursor.execute('DELETE FROM EVENTS WHERE name=?', (name,))
+    conn.commit()
+
+
+def add_schedule(event_id, datatime, name):
+    cursor.execute('INSERT INTO SCHEDULE VALUES (?, ?, ?)', (event_id, datatime, name))
+    conn.commit()
+    
