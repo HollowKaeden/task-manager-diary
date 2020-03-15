2d8b3ba9b0314bfe95ad8238b8209f9187f2fdb9
1213 <1@1>
2020-03-15 12:28:03 +0300

    Коммит

--- GUI.py
+++ GUI.py
@@ -1,1 +1,74 @@
 # Интерфейс
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QComboBox, QTableWidget, QTableWidgetItem
+from PyQt5.QtGui import QFont
+from PyQt5 import QtCore
+import database as db
+
+
+class main_window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.secondWin = None
+        self.initUI()
+
+    def initUI(self):
+        db.connect()
+        font = QFont('Comic sans', 14)
+        self.setGeometry(1000, 600, 717, 600)
+        self.setWindowTitle('Расписание')
+        self.table = QTableWidget(self)
+        self.table.setGeometry(0, 80, 717, 295)
+        self.table.setColumnCount(3)
+        self.table.setHorizontalHeaderLabels(['Название', 'Домашнее задание', 'Заметки'])
+        self.table.setRowCount(9)
+        self.table.setColumnWidth(0, 150)
+        self.table.setColumnWidth(1, 275)
+        self.table.setColumnWidth(2, 275)
+        self.table.activated[str].connect(self.add_lesson)
+        self.btn = QPushButton(self)
+        self.btn.setText('Добавить урок')
+        self.btn.setGeometry(20, 10, 150, 30)
+        self.btn.setFont(font)
+        self.btn.clicked.connect(self.add_lesson)
+        self.btn1 = QPushButton(self)
+        self.btn1.setText('Добавить ДЗ')
+        self.btn1.setGeometry(190, 10, 150, 30)
+        self.btn1.setFont(font)
+        self.btn2 = QPushButton(self)
+        self.btn2.setText('Добавить заметку')
+        self.btn2.setGeometry(360, 10, 170, 30)
+        self.btn2.setFont(font)
+        self.btn3 = QPushButton(self)
+        self.btn3.setText('←')
+        self.btn3.setGeometry(570, 10, 40, 30)
+        self.btn3.setFont(font)
+        self.btn4 = QPushButton(self)
+        self.btn4.setText('→')
+        self.btn4.setGeometry(640, 10, 40, 30)
+        self.btn4.setFont(font)
+        lessons = db.get_lessons('Понедельник')
+        for i in range(9):
+            self.table.setItem(i, 0, QTableWidgetItem(lessons.pop()))
+        self.show()
+
+    def add_lesson(self):
+        print(1)
+
+    def add_homework(self):
+        pass
+
+    def add_notes(self):
+        pass
+
+    def previous_day(self):
+        pass
+
+    def following_day(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = main_window()
+    sys.exit(app.exec_())
--- database.py
+++ database.py
@@ -1,1 +1,42 @@
 # Запросы базе данных
+import sqlite3
+cursor = None
+conn = None
+
+
+def connect():
+    global cursor, conn
+    conn = sqlite3.connect("task_manager_data.db")
+    cursor = conn.cursor()
+
+
+connect()
+
+
+def add_events(name):
+    cursor.execute('INSERT INTO EVENTS (name) VALUES (?)', (name,))
+    conn.commit()
+
+
+def del_events(name):
+    cursor.execute('DELETE FROM EVENTS WHERE name=?', (name,))
+    conn.commit()
+
+
+def add_schedule(event_id, datatime, name):
+    cursor.execute('INSERT INTO SCHEDULE VALUES (?, ?, ?)', (event_id, datatime, name))
+    conn.commit()
+
+
+def get_lessons(date):
+    lessons = list(map(lambda x: x[0], cursor.execute('''SELECT e.name FROM EVENTS e 
+                             INNER JOIN SCHEDULE s
+                             ON e.id = s.event_id
+                             WHERE s.datatime=?''', (date, )).fetchall()))
+    while len(lessons) != 9:
+        lessons.insert(0, '')
+    return lessons
+
+
+connect()
+print(get_lessons('Понедельник'))
